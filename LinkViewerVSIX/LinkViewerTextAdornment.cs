1f9210f61158c7b1bf627f039cb3c2e509c38d5a
Junil Um <powerumc@gmail.com>
2016-02-11 21:10:35 +0900

    basic function impl.

--- LinkViewerVSIX/LinkViewerTextAdornment.cs
+++ LinkViewerVSIX/LinkViewerTextAdornment.cs
@@ -16,6 +16,8 @@ using System.Windows.Media.Imaging;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.IO;
+using System.Windows;
+using System.Windows.Input;
 
 namespace LinkViewerVSIX
 {
@@ -24,36 +26,19 @@ namespace LinkViewerVSIX
     /// </summary>
     internal sealed class LinkViewerTextAdornment
     {
-        /// <summary>
-        /// The layer of the adornment.
-        /// </summary>
         private readonly IAdornmentLayer layer;
-
-        /// <summary>
-        /// Text view where the adornment is created.
-        /// </summary>
         private readonly IWpfTextView view;
-
-        /// <summary>
-        /// Adornment brush.
-        /// </summary>
         private readonly Brush brush;
-
-        /// <summary>
-        /// Adornment pen.
-        /// </summary>
         private readonly Pen pen;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="LinkViewerTextAdornment"/> class.
-        /// </summary>
-        /// <param name="view">Text view to create the adornment for</param>
-        public LinkViewerTextAdornment(IWpfTextView view)
+	    private Image img;
+		public LinkViewerTextAdornment(IWpfTextView view)
         {
             if (view == null)
             {
                 throw new ArgumentNullException("view");
             }
+
+			view.VisualElement.MouseMove += VisualElementOnMouseMove;
 
             this.layer = view.GetAdornmentLayer("LinkViewerTextAdornment");
 
@@ -70,15 +55,28 @@ namespace LinkViewerVSIX
             this.pen.Freeze();
         }
 
-        /// <summary>
-        /// Handles whenever the text displayed in the view changes by adding the adornment to any reformatted lines
-        /// </summary>
-        /// <remarks><para>This event is raised whenever the rendered text displayed in the <see cref="ITextView"/> changes.</para>
-        /// <para>It is raised whenever the view does a layout (which happens when DisplayTextLineContainingBufferPosition is called or in response to text or classification changes).</para>
-        /// <para>It is also raised whenever the view scrolls horizontally or when its size changes.</para>
-        /// </remarks>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event arguments.</param>
+		private Regex regex = new Regex(@"(http|https)[-a-zA-Z0-9:_\+.~#?&//=]{2,256}\.[^@\ ][a-z]{2,12}\b(\/[-a-zA-Z0-9:%_\+.~#?&//=]*)?", RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.Compiled);
+		private void VisualElementOnMouseMove(object sender, MouseEventArgs args)
+	    {
+			var position = args.GetPosition(view.VisualElement);
+			Trace.WriteLine($"X:{position.X}	Y:{position.Y}");
+
+		    var line = view.TextViewLines.GetTextViewLineContainingYCoordinate(position.Y + view.ViewportTop);
+			var text = line?.Extent.GetText();
+			Trace.WriteLine(text);
+			if (line == null) return;
+
+			if (regex.IsMatch(text))
+			{
+				var match = regex.Match(text);
+				ShowImageAsync(match.Value);
+			}
+			else
+			{
+				layer.RemoveAllAdornments();
+			}
+	    }
+
         internal void OnLayoutChanged(object sender, TextViewLayoutChangedEventArgs e)
         {
             foreach (ITextViewLine line in e.NewOrReformattedLines)
@@ -87,23 +85,19 @@ namespace LinkViewerVSIX
             }
         }
 
-        /// <summary>
-        /// Adds the scarlet box behind the 'a' characters within the given line
-        /// </summary>
-        /// <param name="line">Line to add the adornments</param>
         private void CreateVisuals(ITextViewLine line)
         {
-            var regex = new Regex(@"(http|https)[-a-zA-Z0-9:_\+.~#?&//=]{2,256}\.[^@\ ][a-z]{2,12}\b(\/[-a-zA-Z0-9:%_\+.~#?&//=]*)?", RegexOptions.IgnoreCase | RegexOptions.Singleline);
-            var lineText = line.Extent.Snapshot.GetText(Span.FromBounds(line.Start, line.End));
-            if (regex.IsMatch(lineText))
-            {
-                var currentLine = view.GetTextViewLineContainingBufferPosition(view.Caret.Position.BufferPosition);
-                Trace.WriteLine(currentLine.Extent.GetText());
+            //var regex = new Regex(@"(http|https)[-a-zA-Z0-9:_\+.~#?&//=]{2,256}\.[^@\ ][a-z]{2,12}\b(\/[-a-zA-Z0-9:%_\+.~#?&//=]*)?", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            //var lineText = line.Extent.Snapshot.GetText(Span.FromBounds(line.Start, line.End));
+            //if (regex.IsMatch(lineText))
+            //{
+            //    var currentLine = view.GetTextViewLineContainingBufferPosition(view.Caret.Position.BufferPosition);
+            //    Trace.WriteLine(currentLine.Extent.GetText());
 
-                var match = regex.Match(currentLine.Extent.GetText());
-                ShowImageAsync(match.Value);
+            //    var match = regex.Match(currentLine.Extent.GetText());
+            //    ShowImageAsync(match.Value);
 
-            }
+            //}
 
             IWpfTextViewLineCollection textViewLines = this.view.TextViewLines;
 
@@ -139,12 +133,8 @@ namespace LinkViewerVSIX
 
         protected async void ShowImageAsync(string source)
         {
-            var img = new Image
-            {
-                Source = await LoadImageSourceAsync(source)
-            };
-
-            layer.AddAdornment(AdornmentPositioningBehavior.ViewportRelative, null, null, img, null);
+	        img = new Image {Source = await LoadImageSourceAsync(source)};
+	        layer.AddAdornment(AdornmentPositioningBehavior.ViewportRelative, null, null, img, null);
         }
 
         private async Task<ImageSource> LoadImageSourceAsync(string address)
@@ -168,3 +158,4 @@ namespace LinkViewerVSIX
         }
     }
 }
+
